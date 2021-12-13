5a720130e45940edde221024f6713222195addc3
brucewebsterjr <85914450+brucewebsterjr@users.noreply.github.com>
2021-12-13 16:05:17 -0500

    Add files via upload
    Uploading requested plots and the R code that produced them.

---
+++ plot3.R
@@ -0,0 +1,26 @@
+## Reformatting original dataset
+
+data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+## Subsetting the data for the correct dates
+data_sub <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Reformatting the date and time variables
+datetime <- paste(as.Date(data_sub$Date), data_sub$Time)
+data_sub$datetime <- as.POSIXct(datetime)
+
+## Creating the 3rd plot
+with(data_sub, {
+    plot(Sub_metering_1~data_sub$datetime, type="l",
+         ylab="Global Active Power (kilowatts)", xlab="")
+    lines(Sub_metering_2~data_sub$datetime,col='Red')
+    lines(Sub_metering_3~data_sub$datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Saving to a png file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
